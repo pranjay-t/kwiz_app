c3961913412953dca78f0afeb6e10f433ae17269
pranjay-t <pranjayt90@gmail.com>
2025-01-20 20:23:11 +0530

    done

--- lib/core/Secret/secret.dart
+++ lib/core/Secret/secret.dart
@@ -1,5 +1,3 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 class Secret {
-  static String get apiUrl => dotenv.env['API_URL']!;
+  static String get apiUrl => 'https://api.jsonserve.com/Uw5CrX';
 }
